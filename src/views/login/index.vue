c36e2d289eae8264f4c85a687209b183326fdf88
bbz294117 <1277950938@qq.com>
2024-09-21 01:32:52 +0800

    乱七八糟的依赖

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,31 @@
 <template>
     <div class="login">
-        登录页面
+        <div class="login-con">
+            <ul class="menu-tab">
+            <li v-for="(v) in MenueDate" :class="{current :v.current}" :key="v.type" @click="clickMenu(v)">
+                {{ v.txt }}
+            </li>
+        </ul>
+        </div>
     </div>
 
 </template>
 
-<script>
-export default{
+<script lang="ts" setup>
 
-}
+    import { reactive } from 'vue';
 
+    const MenueDate=reactive([
+        {txt:"登录",current:true, type:"login"},
+        {txt:"注册",current:false, type:"login"}
+    ])
+
+    let clickMenu=(item:any)=>{
+        MenueDate.forEach((elemt)=>{
+            elemt.current=false
+        })
+        item.current =true
+    }
 
 </script>
 
@@ -18,8 +34,27 @@ export default{
     background-color:rgb(147, 147, 220);
     height:100%;
 }
-html,body,#app{
+html,
+body,
+#app{
     height:100%;
+}
+.menu-tab{
+    text-align: center;
+    li{
+        display: inline-block;
+        width: 88px;
+        line-height: 36px;
+        font-size: 14px;
+        color: aliceblue;
+        border-radius: 2px;
+        cursor: pointer;
+
+    }
+
+    .current{
+        background-color: aqua;
+    }
 }
 
 </style>
