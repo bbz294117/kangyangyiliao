869d345bdc53cebd8a40ac29670c85a087645ac5
bbz294117 <1277950938@qq.com>
2024-09-29 22:18:28 +0800

    路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,9 +5,27 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/login',
     name: 'login',
-
     component: () => import('../views/login/index.vue')
-  }
+
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/login/register.vue')
+
+  },
+  {
+    path: '/test',
+    name: 'test',
+    component: () => import('../views/login/test.vue')
+
+  },
+  {
+    path: '/main',
+    name: 'main',
+    component: () => import('../views/main/mainMage.vue')
+
+  },
 ]
 
 const router = createRouter({
