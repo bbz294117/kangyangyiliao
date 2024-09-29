5474d9db251a92b02d7a53c5a0d108b2e4c40850
bbz294117 <1277950938@qq.com>
2024-09-29 22:17:49 +0800

    跳转测试

---
+++ src/views/login/test.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function goNews() {
+  router.push("/register");
+}
+</script>
+
+<template>
+	<button @click="goNews">跳转新闻</button>
+</template>
