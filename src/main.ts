4a90faf5d8e1603f4121dda778e4c204eb19867c
bbz294117 <1277950938@qq.com>
2024-12-01 16:22:05 +0800

    修改

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'element-ui/lib/theme-chalk/index.css';
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 
 createApp(App).use(store).use(ElementPlus).use(router).mount('#app')
+
+
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  createApp(App).component(key, component)
+}
