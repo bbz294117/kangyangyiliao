5eaa5d7b4b63e532f06dfa2e7dc5f05322fe5391
bbz294117 <1277950938@qq.com>
2024-12-06 22:52:33 +0800

    测试页面修改

--- src/views/login/test.vue
+++ src/views/login/test.vue
@@ -1,13 +1,42 @@
-<script setup lang="ts">
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-
-function goNews() {
-  router.push("/register");
-}
-</script>
-
 <template>
-	<button @click="goNews">跳转新闻</button>
+  <div ref="chartDom" style="width: 100%; height: 400px;"></div>
 </template>
+<!--
+
+<script lang="ts">
+import echarts from 'echarts/types/dist/echarts';
+import { onMounted, nextTick, ref } from 'vue';
+
+export default {
+  setup() {
+    const chartDom = ref<HTMLElement | null>(null);
+    const chartOptions = ref({
+      title: {
+        text: 'ECharts 示例'
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['A', 'B', 'C', 'D', 'E']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10]
+      }]
+    });
+
+    onMounted(() => {
+      nextTick(() => {
+        if (chartDom.value) {
+          const myChart = echarts.init(chartDom.value);
+          myChart.setOption(chartOptions.value);
+        }
+      });
+    });
+
+    return { chartDom, chartOptions };
+  }
+};
+
+</script> -->
